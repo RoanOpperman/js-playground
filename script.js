a223db4540049d2d04f13b6bd2bc6f8f6333ba82
RoanOpperman <17789917+RoanOpperman@users.noreply.github.com>
2024-07-27 22:54:51 -0600

    show practice if/else(...)

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 "use strict";
-let linkedTestElement = document.getElementById("linked-test").innerHTML;
-linkedTestElement = document.getElementById(
-  "linked-test"
-).innerHTML = `${linkedTestElement}--JS: linked--`;
-console.log(linkedTestElement);
+// let linkedTestElement = document.getElementById("linked-test").innerHTML;
+// linkedTestElement = document.getElementById(
+//   "linked-test"
+// ).innerHTML = `${linkedTestElement}--JS: linked--`;
+// console.log(linkedTestElement);
+// **un-comment out the section above**
+
+// | **********    if/else    ***********
+
+const noRepeat = "a string";
+let repeatVar = null;
+let checkProperty = false;
+if ((checkProperty = false)) {
+  repeatVar = "property one";
+  console.log(`${noRepeat} + condition ONE met`);
+} else if ((checkProperty = false)) {
+  repeatVar = "property two";
+  console.log(`${noRepeat} + condition TWO met`);
+} else if ((checkProperty = true)) {
+  repeatVar = "property three";
+  console.log(`${noRepeat} + condition THREE met`);
+} else if ((checkProperty = false)) {
+  repeatVar = "property four";
+  console.log(`${noRepeat} + condition FOUR met`);
+} else {
+  console.log("default condition");
+}
